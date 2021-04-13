8e839af31082eb110f1ea978e29e43cb28b71d53
eqrakhattak <eqra.khattak7@gmail.com>
2021-04-13 21:06:50 +0500

    Lab assignment 1

---
+++ Game of Life/Assets/movedany.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movedany : MonoBehaviour
+{
+    public float speed = 4;
+    public Rigidbody2D rigidbody1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody1 = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move = Input.GetAxis("Horizontal");
+        rigidbody1.velocity = new Vector2(move * speed, 0);
+    }
+}
